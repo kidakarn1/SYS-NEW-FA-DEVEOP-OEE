b8c3fbbadf8fb2f2b2dbdbe85960756c442ef644
TBKT-SYSTEM <117958477+TBKT-SYSTEM@users.noreply.github.com>
2024-11-29 11:43:54 +0700

    Ready Start Version 2.0.8

--- TBK FA System/MainFrm.designer.vb
+++ TBK FA System/MainFrm.designer.vb
@@ -97,7 +97,7 @@ Partial Class MainFrm
         Me.Label9.Name = "Label9"
         Me.Label9.Size = New System.Drawing.Size(179, 19)
         Me.Label9.TabIndex = 23
-        Me.Label9.Text = "FA VERSIONS 2.0.7"
+        Me.Label9.Text = "FA VERSIONS 2.0.8"
         '
         'Panel1
         '
